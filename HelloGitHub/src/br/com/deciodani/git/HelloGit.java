9978826177422c630acd2e0add13ebe89cbfaa2b
deciodani <62177400+deciodani@users.noreply.github.com>
2022-05-20 17:34:13 -0300

    Add files via upload

---
+++ HelloGitHub/src/br/com/deciodani/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.deciodani.git;
+
+/**
+ * @author decio
+ * Exemplo de integração do eclipse com githuh
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello git");
+	}
+
+}
